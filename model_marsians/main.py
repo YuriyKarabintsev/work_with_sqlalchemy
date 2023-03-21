e618edac6ba53516ab938cfe3b2bf104b36f1a92
YuriyKarabintsev <114873039+YuriyKarabintsev@users.noreply.github.com>
2023-03-21 12:07:42 +0700

    Update main.py

--- model_marsians/main.py
+++ model_marsians/main.py
@@ -1,25 +1,20 @@
-from flask import Flask
-from model_marsians.data import db_session
-from data.users import Jobs
+from data import db_session
+from flask import render_template
+from data.jobs import Jobs
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+#app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 def main():
+    app.run()
+
+@app.route("/")
+def index():
     db_session.global_init("db/jobs.db")
-
-    j = Jobs()
-    j.team_leader = 1
-    j.job = "deployment of residential modules 1 and 2"
-    j.work_size = 15
-    j.collaborators = "2, 3"
-    j.start_date = "now"
-    j.is_finished = False
-
     db_sess = db_session.create_session()
-    db_sess.add(j)
-    db_sess.commit()
+    jobs = db_sess.query(Jobs).all()
+    return render_template("prof.html", jobs=jobs)
 
 
 if __name__ == '__main__':
